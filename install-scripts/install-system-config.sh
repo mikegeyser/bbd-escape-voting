e0f6bde95eff2d720bb31a9466e3a49ff9c35e8f
Evert Geldenhuys <evert.geldenhuys2@gmail.com>
2017-07-13 14:28:35 +0200

    Fix apache config path

--- install-scripts/install-system-config.sh
+++ install-scripts/install-system-config.sh
@@ -2,5 +2,5 @@
 # TO BE RUN FROM ROOT FOLDER!
 
 sudo cp configs/010_pi-nopasswd /etc/sudoers.d/
-sudo cp configs/serve-cgi-bin.conf etc/apache2/conf-enabled
+sudo cp configs/serve-cgi-bin.conf /etc/apache2/conf-enabled
 sudo cp configs/apache2.conf /etc/apache2
