1998c29ed1eb910093a264ef15022aa35f74013a
Evert Geldenhuys <evert.geldenhuys2@gmail.com>
2017-07-13 14:08:19 +0200

    Force uninstall

--- install-scripts/uninstall-packages.sh
+++ install-scripts/uninstall-packages.sh
@@ -2,4 +2,4 @@
 
 sudo apt-get remove apache2 apache2-utils libapache2-mod-php5 \
 php5 php-pear php5-xcache php5-mysql php5-curl php5-gd \
-mysql-server mysql-client dnsmasq hostapd iptables-persistent
+mysql-server mysql-client dnsmasq hostapd iptables-persistent --force-yes
