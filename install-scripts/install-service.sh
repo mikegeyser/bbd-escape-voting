980513ee1fd8bf200d3959214cc69187b143db61
Evert Geldenhuys <evert.geldenhuys2@gmail.com>
2017-07-13 17:51:32 +0200

    systemd service

---
+++ install-scripts/install-service.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# TO BE EXECUTED FROM ROOT FOLDER!
+
+controlDir=$PWD/control/accesspoint
+webControlDir=$PWD/control/web
+
+echo "[Unit]
+Description=BBD Escape Hotspot and PHP loop
+
+[Service]
+Type=oneshot
+RemainAfterExit=yes
+ExecStart=$webControlDir/initPHPLoop.sh
+ExecStart=$webControlDir/startPHPLoop.sh
+ExecStart=$controlDir/setSSID.sh &
+ExecStart=$controlDir/accesspoint.sh &
+ExecStart=$controlDir/startAP.sh &
+ExecStop=$controlDir/stopAP.sh &
+ExecStop=$controlDir/stopAP.sh &
+ExecStop=$webControlDir/stopPHPLoop.sh
+ExecStop=killall -9 apache2
+ExecStop=systemctl restart appache2
+
+[Install]
+WantedBy=multi-user.target" > escape.service
+
+sudo cp escape.service /etc/systemd/system/
+sudo systemctl enable escape.service
+echo 'Escape.service has been enabled'
--- install.sh
+++ install.sh
@@ -6,4 +6,7 @@ sudo install-scripts/install-web-files.sh
 sudo install-scripts/createDB.sh
 sudo install-scripts/install-web-config.sh
 sudo install-scripts/setup-camera.sh
+sudo install-scripts/createDB.sh
+sudo install-scripts/install-service.sh
+
 sudo reboot
