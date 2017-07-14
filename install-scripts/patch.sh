ab05306774d8ad55df6dc039e94045e215bb0734
Evert Geldenhuys <evert.geldenhuys2@gmail.com>
2017-07-14 08:07:43 +0200

    More fixes

--- install-scripts/patch.sh
+++ install-scripts/patch.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 # TO BE RUN FROM ROOT AS PART OF INSTALL
+# Create accesspoint.sh
 
 echo "#!/bin/bash
 rootDir=$PWD" > $PWD/control/accesspoint/accesspoint.sh
+
 echo "$(cat $PWD/control/accesspoint/accesspoint.sh.template)" >> $PWD/control/accesspoint/accesspoint.sh
 sudo chmod 774 $PWD/control/accesspoint/accesspoint.sh
