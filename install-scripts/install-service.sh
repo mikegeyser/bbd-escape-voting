d1706e2c53eca30cd1351cbe0c252f43fb225a2e
Evert Geldenhuys <evert.geldenhuys2@gmail.com>
2017-08-14 22:35:29 +0200

    Remove old install scripts. Add logging to service

--- install-scripts/install-service.sh
+++ install-scripts/install-service.sh
@@ -5,21 +5,26 @@
 controlDir=$PWD/control/accesspoint
 webControlDir=$PWD/control/web
 
+logFile=/var/log/escape/service.log
+sudo mkdir -p /var/log/escape
+
+# Start.sh
 echo "#!/bin/bash
-sudo systemctl restart apache2
-sudo systemctl start mysql
-$webControlDir/initPHPLoop.sh &
-$webControlDir/startPHPLoop.sh &
-$controlDir/accesspoint.sh &
-$controlDir/startAP.sh &" > $PWD/control/start.sh
+sudo systemctl restart apache2 >> ${logFile} 2>&1
+sudo systemctl start mysql >> ${logFile} 2>&1
+$webControlDir/initPHPLoop.sh >> ${logFile} 2>&1 &
+$webControlDir/startPHPLoop.sh >> ${logFile} 2>&1 &
+$controlDir/accesspoint.sh >> ${logFile} 2>&1 &
+$controlDir/startAP.sh >> ${logFile} 2>&1 &" > $PWD/control/start.sh
 sudo chmod 774 $PWD/control/start.sh
 
+# Stop.sh
 echo "#!/bin/bash
-$controlDir/stopAP.sh &
-$controlDir/stopAP.sh &
-$webControlDir/stopPHPLoop.sh &
-sudo /usr/bin/killall -9 apache2
-sudo /bin/systemctl stop apache2
+$controlDir/stopAP.sh >> ${logFile} 2>&1 &
+$controlDir/stopAP.sh >> ${logFile} 2>&1 &
+$webControlDir/stopPHPLoop.sh >> ${logFile} 2>&1 &
+sudo /usr/bin/killall -9 apache2 >> ${logFile} 2>&1
+sudo /bin/systemctl stop apache2 >> ${logFile} 2>&1
 " > $PWD/control/stop.sh
 sudo chmod 774 $PWD/control/stop.sh
 
