e2f77ea644f388e6dc06b66efef30bc72b871f4f
Evert Geldenhuys <evert.geldenhuys2@gmail.com>
2017-08-14 23:18:03 +0200

    Flush iptables

--- install-scripts/install-system-config.sh
+++ install-scripts/install-system-config.sh
@@ -5,5 +5,13 @@ sudo cp configs/010_pi-nopasswd /etc/sudoers.d/
 sudo cp configs/serve-cgi-bin.conf /etc/apache2/conf-enabled
 sudo cp configs/apache2.conf /etc/apache2
 
+sudo iptables -P INPUT ACCEPT
+sudo iptables -P FORWARD ACCEPT
+sudo iptables -P OUTPUT ACCEPT
+
+sudo iptables -t nat -F
+sudo iptables -t mangle -F
+sudo iptables -F
+sudo iptables -X
+
 sudo iptables -t nat -A POSTROUTING -o wlan0 -j MASQUERADE
-sudo iptables -t nat -A PREROUTING -p tcp -m tcp --dport 80 -j DNAT --to-destination 172.24.1.1
