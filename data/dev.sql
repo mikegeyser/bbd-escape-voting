ad96d849d537f0e3df4e242d81a252e0e98c16b6
Mike Geyser <michaelg@bbd.co.za>
2017-08-21 12:49:05 +0200

    Removed FK constraints

--- data/dev.sql
+++ data/dev.sql
@@ -1,7 +1,13 @@
 use Escape;
+
+SET FOREIGN_KEY_CHECKS = 0;
+
 truncate table COMMENTS;
 truncate table VOTES;
 truncate table PRESENTER;
+
+SET FOREIGN_KEY_CHECKS = 1;
+
 
 insert into PRESENTER (HANDLE, NAME, TOPIC, START, END) values ("@WernerMostert1", "Werner Mostert", "Something about Kubernetes", "2017-08-31 14:00:00", "2017-08-31 15:00:00");
 insert into PRESENTER (HANDLE, NAME, TOPIC, START, END) values ("@zaydkara", "Zayd Kara", "Something about Docker", "2017-08-31 15:00:00", "2017-08-31 16:00:00");
